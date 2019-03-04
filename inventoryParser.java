505a5c8abf2a5d6525c9552c90f6a73cae5904fb
Mohit Marwari <mmarwadi@gmail.com>
2019-03-04 01:43:31 -0500

    Update inventoryParser.java

--- inventoryParser.java
+++ inventoryParser.java
@@ -1,20 +1,20 @@
 public class InventoryParser {
 
         public static voig main(String[] args) {
-
+                // entry point of our app
         }
 
 
         private bool isCsv(File file) {
-
+                // checks the file name extension and returns True if the file is of csv extension; else False
         }
 
         private bool isTabDelimited(File file) {
-
+                // checks the file name extension and returns True if the file is of tab delimited extension; else False
         }
 
         private bool isExcel(File file) {
-
+                // checks the file name extension and returns True if the file is of xml/xmls extension; else False
         }
 
         public void updateInventory(File directory) {
@@ -26,7 +26,9 @@ public class InventoryParser {
                                 type = 'tab';
                         if (isExcel(fileEntry)
                                 type = 'excel';
-                        FileParser parsedFile = new FileParser(fileEntry,type);        
+                        // create object of our FileParser class
+                        FileParser parsedFile = new FileParser(fileEntry, type);
+                        parsedFile.parse();
                 }
         }
 }
