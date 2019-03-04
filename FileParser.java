e05e76a13ef0cac2eb8de587e7189654dd7c5dda
Mohit Marwari <mmarwadi@gmail.com>
2019-03-04 01:46:08 -0500

    Update FileParser.java

--- FileParser.java
+++ FileParser.java
@@ -11,6 +11,9 @@ public class FileParser {
     private void writeToDb( String productId, int quantity) {
             //query to store supplier id, product id and quantity to our inventory table.
             //supplier id is retrieved from constructor.
+            // first runs a query to identify if primary key (supplierId, productId) exists in the table;
+            // if it exists then it will run another query to update the quantity corresponding to that primary key
+            // if the concerned key didn't exist, then we will just create this new entry with the concered quantity.
     }
     public void parse() {
         // will read the file from reader like BufferReader.
